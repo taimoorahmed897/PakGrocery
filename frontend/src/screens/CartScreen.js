7bfafc1a1fcce1d356d0969426869ffbc07dfdc9
taimoorahmed897 <45800065+taimoorahmed897@users.noreply.github.com>
2021-01-22 19:03:29 +0500

    Semester project eccomerce Pakgrocery

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addToCart } from '../actions/cartActions';
 
 function CartScreen(props){
+        const cart = useSelector(state => state.cart);
+
+        const { cartItems } = cart;
 const productId=props.match.params.id
 const qty=props.location.search? Number(props.location.search.split("=")[1]):1;
 const dispatch=useDispatch()
@@ -29,7 +32,7 @@ return<div className="cart">
                         <div>
                                 Cart is empty!
                         </div>:
-                        cartItems.map(items=>
+                        cartItems.map(item=>
                                 <div>
                                         <img src={item.img} alt="product"></img>
                                         <div className="cart-name">
@@ -37,8 +40,16 @@ return<div className="cart">
                                                         {item.name}
                                                 </div>
                                                 <div>
-                                                        <select></select>
+                                                        Qty:
+                                                        <select>
+                                                                <option value="1">1</option>
+                                                                <option value="2">2</option>
+                                                                <option value="3">3</option>
+                                                        </select>
                                                 </div>
+                                        </div>
+                                        <div>
+                                                {item.price}
                                         </div>
                                 </div>
                                 )
